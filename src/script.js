a17dd18e26fa163bafe6a19da0b7905e4fdaa4d2
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-19 11:58:16 -0600

    Put api and render service all on one page

--- src/script.js
+++ src/script.js
@@ -15,3 +15,40 @@ document.querySelector("form").addEventListener("submit", async (event) => {
 
   // renderService.renderWeather(city, data);
 });
+
+// api.serivce
+// const BASE_URL = "https://api.openweathermap.org";
+
+// export default {
+//   getWeatherResponse(currentWeather) {
+//     return fetch(
+//       // need lat, lon, and api key
+//       `${BASE_URL}/data/${currentWeather}/forecast?lat={lat}&lon={lon}&appid={API key}`
+//     );
+//   },
+
+//   async getWeatherByCity(cityName) {
+//     const response = await fetch(
+//       // need city name, state code, and country code
+//       `${BASE_URL}/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}`
+//     );
+//     // promise awaited by caller
+//     return response.json();
+//   },
+
+//   async getForecastByCity(futureWeather) {
+//     const response = await fetch(
+//       `${BASE_URL}/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+//     );
+
+//     // promise awaited by caller
+//     return response.json();
+//   },
+// };
+
+// render.service
+// const resultsList = document.querySelector("#results ul");
+// const resultsH2Span = document.querySelector("#current");
+
+// const futureList = document.querySelector("#future ul");
+// const futureH2Span = document.querySelector("#forecast");
--- src/services/render.service.js
+++
@@ -1,5 +0,0 @@
-// const resultsList = document.querySelector("#results ul");
-// const resultsH2Span = document.querySelector("#current");
-
-// const futureList = document.querySelector("#future ul");
-// const futureH2Span = document.querySelector("#forecast");
