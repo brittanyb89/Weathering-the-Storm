6b29e58c61361b392623452957627d34aa060b1e
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-19 11:21:19 -0600

    Comment out fetch and response

--- src/services/render.service.js
+++ src/services/render.service.js
@@ -1,5 +1,5 @@
-const resultsList = document.querySelector("#results ul");
-const resultsH2Span = document.querySelector("#current");
+// const resultsList = document.querySelector("#results ul");
+// const resultsH2Span = document.querySelector("#current");
 
-const futureList = document.querySelector("#future ul");
-const futureH2Span = document.querySelector("#forecast");
+// const futureList = document.querySelector("#future ul");
+// const futureH2Span = document.querySelector("#forecast");
