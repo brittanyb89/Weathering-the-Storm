418978e502bd38c03b368561dbd6fb4258d8fc8b
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-19 11:16:46 -0600

    Declared variables  and added id to html div tags

---
+++ src/services/render.service.js
@@ -0,0 +1,5 @@
+const resultsList = document.querySelector("#results ul");
+const resultsH2Span = document.querySelector("#current");
+
+const futureList = document.querySelector("#future ul");
+const futureH2Span = document.querySelector("#forecast");
