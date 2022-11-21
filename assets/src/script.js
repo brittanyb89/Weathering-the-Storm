eb3ea6d390438fff0257daae67d234a578775aeb
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-21 16:04:06 -0600

    Added jquery tags to html

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,72 +1,78 @@
-import {
-  forecastSection,
-  form,
-  searchHistorySection,
-  currentWeatherSection,
-} from "./lib";
-import apiService from "./services/api.service";
-import renderService from "./services/render.service";
-
 // declare global variables
+const apiKEY = "e44adac92450ec69c542cdd83e3a48b0";
 let currentCity = "";
 let prevCity = "";
-let coords;
-let currentWeather;
-let forecastData;
+// let coords;
+// let currentWeather;
+// let forecastData;
 
-// Activate search button
-document.querySelectorAll("button").forEach((button) => {
-  button.classList.add("button");
-});
+// display current conditions of searched city
+let getCurrentWeather = (event) => {
+  let cityName = $("#citySearch").val();
+  currentCity = $("#citySearch").val();
 
-// submit form information
-document.querySelector("form").addEventListener("submit", async (event) => {
-  event.preventDefault();
+  const BASE_URL = "https://api.openweathermap.org";
+  const requestURL = `"${BASE_URL}/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + apiKEY;`;
 
-  let cityName = event.target.city.value;
+  fetch(requestURL).then((response) => {
+    return response.json();
+  });
+};
+// import {
+//   forecastSection,
+//   form,
+//   searchHistorySection,
+//   currentWeatherSection,
+// } from "./lib";
+// import apiService from "./services/api.service";
+// import renderService from "./services/render.service";
 
-  let forecastData = await getWeatherByCity(cityName);
+// // Activate search button
+// document.querySelectorAll("button").forEach((button) => {
+//   button.classList.add("button");
+// });
 
-  renderService.renderCoords(cityName, forecastData);
-});
+// // submit form information
+// document.querySelector("form").addEventListener("submit", async (event) => {
+//   event.preventDefault();
 
-// Add history to local storage
-const historyStorage = JSON.parse(localStorage.getItem("searchHistory")) || [];
+//   let cityName = event.target.city.value;
 
-// search and display the current weather by grabbing searched city from search box and adding to search history
-function addCityToHistoryStorage(cityName, coords) {
-  // to avoid duplicate city search in history section
-  const cityNameUppercase = cityName.toUppercase();
-  if (!historyStorage.find((city) => city.name === cityNameUppercase)) {
-    historyStorage.push({ name: cityNameUppercase, coords });
-    localStorage.setItem("searchHistory", JSON.stringify(historyStorage));
-  }
-}
+//   let forecastData = await getWeatherByCity(cityName);
 
-async function renderWeather(coords) {
-  currentWeather = await apiService.getCurrentWeather(coords);
-  forecastData = await apiService.getForecastData(coords);
+//   renderService.renderCoords(cityName, forecastData);
+// });
 
-  renderService.renderWeather(currentWeather, currentWeatherSection);
-  renderService.renderForcast(forecastData, forecastSection);
-}
+// // Add history to local storage
+// const historyStorage = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
-renderService.renderHistoryButtons(historyStorage, searchHistorySection);
+// // search and display the current weather by grabbing searched city from search box and adding to search history
+// function addCityToHistoryStorage(cityName, coords) {
+//   // to avoid duplicate city search in history section
+//   const cityNameUppercase = cityName.toUppercase();
+//   if (!historyStorage.find((city) => city.name === cityNameUppercase)) {
+//     historyStorage.push({ name: cityNameUppercase, coords });
+//     localStorage.setItem("searchHistory", JSON.stringify(historyStorage));
+//   }
+// }
+
+// async function renderWeather(coords) {
+//   currentWeather = await apiService.getCurrentWeather(coords);
+//   forecastData = await apiService.getForecastData(coords);
+
+//   renderService.renderWeather(currentWeather, currentWeatherSection);
+//   renderService.renderForcast(forecastData, forecastSection);
+// }
+
+// renderService.renderHistoryButtons(historyStorage, searchHistorySection);
 
 // form.addEventListener("submit", async (event) => {});
 
-// searchHistorySection.addEventListener("click", async (event) => {});
+// searchHistorySection.addEventListener("click", async (event) => {
 
 // });
 
-document
-  .getElementById("citySearch")
-  .addEventListener("click", async (event) => {
-    if (event.target.city.value === "BUTTON") {
-      const citySearch = event.target.innerText.toLowercase();
-      let data = await apiService.getCoordsByCityName(forecastData);
-    }
-  });
+// });
 
 // render.service
 // const resultsList = document.querySelector("#results ul");
--- assets/src/services/api.service.js
+++ assets/src/services/api.service.js
@@ -1,31 +1,29 @@
-const BASE_URL = "https://api.openweathermap.org";
-const apiKEY = "e44adac92450ec69c542cdd83e3a48b0";
-const apiCoords = `${BASE_URL}/geo/1.0/direct`;
-const apiWeather = `${BASE_URL}/data/2.5/weather`;
-const apiForecast = `${BASE_URL}/data/2.5/forecast`;
+// const apiCoords = `${BASE_URL}/geo/1.0/direct`;
+// const apiWeather = `${BASE_URL}/data/2.5/weather`;
+// const apiForecast = `${BASE_URL}/data/2.5/forecast`;
 
-// API call api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-export default {
-  async getCoords(city) {
-    return fetch(apiCoords)
-  },
-  async getCurrentWeather(coords) {
+// // API call api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// export default {
+//   async getCoords(city) {
+//     return fetch(apiCoords)
+//   },
+//   async getCurrentWeather(coords) {
+//     let await
+//   },
+//   async getForecastData(coords) {
+//     const response = await fetch();
 
-  },
-  async getForecastData(coords) {
-    const response = await fetch();
-
-    // 40 entries on 'list' -8 per day * 5 days; we only want to use noon entires in this example, but change according to preferrence
-    const data = await response.json();
-    // object destructuring and renaming; in this case, it grans only noon forecast and rename it. found in network tab, click forecast and view response
-    return data.list.filter(({ dt_txt: dateTxt }) => );
-  },
-  //   getWeatherResponse(currentWeather) {
-  //     return fetch(
-  //       // need lat, lon, and api key
-  //       `${BASE_URL}/data/${currentWeather}/forecast?lat={lat}&lon={lon}&appid={API key}`
-  //     );
-};
+// 40 entries on 'list' -8 per day * 5 days; we only want to use noon entires in this example, but change according to preferrence
+// const data = await response.json();
+// object destructuring and renaming; in this case, it grans only noon forecast and rename it. found in network tab, click forecast and view response
+//   return data.list.filter(({ dt_txt: dateTxt }) => );
+// },
+//   getWeatherResponse(currentWeather) {
+//     return fetch(
+//       // need lat, lon, and api key
+//       `${BASE_URL}/data/${currentWeather}/forecast?lat={lat}&lon={lon}&appid={API key}`
+//     );
+// };
 
 //   async getWeatherByCity(cityName) {
 //     const response = await fetch(
