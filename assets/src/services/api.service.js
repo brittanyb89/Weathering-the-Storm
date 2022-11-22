f95c7772d885d0955682eef8b5dcc05c0e7e6a0b
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-21 22:13:23 -0600

    Cleaned up commented out code

--- assets/src/services/api.service.js
+++ assets/src/services/api.service.js
@@ -31,34 +31,3 @@ export default {
     return await response.json();
   },
 };
-
-// 40 entries on 'list' -8 per day * 5 days; we only want to use noon entires in this example, but change according to preferrence
-// const data = await response.json();
-// object destructuring and renaming; in this case, it grans only noon forecast and rename it. found in network tab, click forecast and view response
-//   return data.list.filter(({ dt_txt: dateTxt }) => );
-// },
-//   getWeatherResponse(currentWeather) {
-//     return fetch(
-//       // need lat, lon, and api key
-//       `${BASE_URL}/data/${currentWeather}/forecast?lat={lat}&lon={lon}&appid={API key}`
-//     );
-// };
-
-//   async getWeatherByCity(cityName) {
-//     const response = await fetch(
-//       // need city name, state code, and country code
-//       `${BASE_URL}/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}`
-//     );
-//     // promise awaited by caller
-//     return response.json();
-//   },
-
-//   async getForecastByCity(futureWeather) {
-//     const response = await fetch(
-//       `${BASE_URL}/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
-//     );
-
-//     // promise awaited by caller
-//     return response.json();
-//   },
-// };
--- assets/src/services/render.service.js
+++ assets/src/services/render.service.js
@@ -1,42 +1,42 @@
-// function createCurrentWeatherHeader({ cityName, description, icon }) {
-//   const header = document.createElement("header");
-//   const title = document.createElement("h2");
-//   const iconEl = document.createElement("img");
+function createCurrentWeatherHeader({ cityName, description, icon }) {
+  const header = document.createElement("header");
+  const title = document.createElement("h2");
+  const iconEl = document.createElement("img");
 
-//   iconEl.src = `http://openweathermap.org/img/wn/${icon}.png`;
-//   icon.alt = description;
+  iconEl.src = `http://openweathermap.org/img/wn/${icon}.png`;
+  icon.alt = description;
 
-//   header.appendchild(title);
-//   header.appendChild(iconEl);
+  header.appendchild(title);
+  header.appendChild(iconEl);
 
-//   return header;
-// }
+  return header;
+}
 
-// function createWeatherH2() {
-//   const h2 = document.createElement("h2");
-//   h2.textContent = "5-Day Forecast";
+function createWeatherH2() {
+  const h2 = document.createElement("h2");
+  h2.textContent = "5-Day Forecast";
 
-//   return h2;
-// }
+  return h2;
+}
 
-// function createForecastList(forecastData) {}
+function createForecastList(forecastData) {}
 
-// function createWeatherList({ temp, windSpeed, humidity }) {}
+function createWeatherList({ temp, windSpeed, humidity }) {}
 
-// export default {
-//   renderCurrentWeather(weather, el) {
-//     el.innerHTML = "";
-//     // destructuring and renaming object
-//     const { name: cityName, main, weather: weatherData } = weather;
-//     const { temp, humidity } = main;
-//     const { description, icon } = weatherData[0];
-//     const { speed: windSpeed } = weather.wind;
+export default {
+  renderCurrentWeather(weather, el) {
+    el.innerHTML = "";
+    // destructuring and renaming object
+    const { name: cityName, main, weather: weatherData } = weather;
+    const { temp, humidity } = main;
+    const { description, icon } = weatherData[0];
+    const { speed: windSpeed } = weather.wind;
 
-//     el.appendchild(createCurrentWeatherHeader({ cityName, description, icon }));
-//     el.appendchild(createWeatherList({ temp, windSpeed, humidity }));
-//     // tailwind to give top to bottom effect
-//     // el.classList.add("border-y");
-//   },
-//   renderForecast(forecast, el) {},
-//   renderHistoryButtons(history, el) {},
-// };
+    el.appendchild(createCurrentWeatherHeader({ cityName, description, icon }));
+    el.appendchild(createWeatherList({ temp, windSpeed, humidity }));
+    // tailwind to give top to bottom effect
+    // el.classList.add("border-y");
+  },
+  renderForecast(forecast, el) {},
+  renderHistoryButtons(history, el) {},
+};
