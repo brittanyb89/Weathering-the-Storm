d4365a17bd221d7692b99f0ad5db61ef81c9fa5d
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-30 14:36:57 -0600

    Deleted unneeded files and folders

--- assets/src/lib.js
+++
@@ -1,4 +0,0 @@
-export const form = document.querySelector("form");
-export const searchHistorySection = document.getElementById("searchHistory");
-export const forecastSection = document.getElementById("forecast");
-export const currentWeatherSection = document.getElementById("currentWeather");
