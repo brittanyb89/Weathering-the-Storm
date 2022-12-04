a1c663db1b4748f4d661500bb47a4f63b92a92ae
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-12-04 15:10:29 -0600

    Rearranged code in file

--- assets/src/script.js
+++ assets/src/script.js
@@ -3,88 +3,22 @@ const searchHistorySection = document.getElementById("searchHistory");
 const forecastSection = document.getElementById("forecast");
 const currentWeatherSection = document.getElementById("currentWeather");
 const apiKEY = "e44adac92450ec69c542cdd83e3a48b0";
+const BASE_URL = "https://api.openweathermap.org";
 
-// Add history to local storage
-const historyStorage = JSON.parse(localStorage.getItem("searchHistory")) || [];
-
-function createCurrentWeatherHeader({ cityName, description, icon }) {
-  const header = document.createElement("header");
-  const title = document.createElement("h2");
-  const iconEl = document.createElement("img");
-
-  iconEl.src = `http://openweathermap.org/img/wn/${icon}.png`;
-  icon.alt = description;
-
-  header.appendchild(title);
-  header.appendChild(iconEl);
-
-  return header;
-}
-
-function createWeatherH2() {
-  const h2 = document.createElement("h2");
-  h2.textContent = "5-Day Forecast";
-
-  return h2;
-}
-
-function createForecastList(forecastData) {}
-
-function createWeatherList({ temp, windSpeed, humidity }) {}
-
-function renderCurrentWeather(weather, el) {
-  el.innerHTML = "";
-  // destructuring and renaming object
-  const { name: cityName, main, weather: weatherData } = weather;
-  const { temp, humidity } = main;
-  const { description, icon } = weatherData[0];
-  const { speed: windSpeed } = weather.wind;
-
-  el.appendchild(createCurrentWeatherHeader({ cityName, description, icon }));
-  el.appendchild(createWeatherList({ temp, windSpeed, humidity }));
-  // tailwind to give top to bottom effect
-  // el.classList.add("border-y");
-}
-function renderForecast(forecast, el) {}
-function renderHistoryButtons(history, el) {}
-
-// search and display the current weather by grabbing searched city from search box and adding to search history
-function addCityToHistoryStorage(cityName, coords) {
-  // to avoid duplicate city search in history section
-  const cityNameUppercase = cityName.toUppercase();
-  if (!historyStorage.find((city) => city.name === cityNameUppercase)) {
-    historyStorage.push({ name: cityNameUppercase, coords });
-    localStorage.setItem("searchHistory", JSON.stringify(historyStorage));
-  }
-}
-
-async function renderForecast(coords) {
-  currentWeather = await apiService.getCurrentWeather(coords);
-  forecastData = await apiService.getForecastData(coords);
-
-  renderWeather(currentWeather, currentWeatherSection);
-  renderCurrentWeather(forecastData, forecastSection);
-}
-
-renderHistoryButtons(historyStorage, searchHistorySection);
-
-form.addEventListener("submit", async (event) => {});
-
-searchHistorySection.addEventListener("click", async (event) => {});
-
-// declare global variables
-let currentCity = "";
 let prevCity = "";
 let coords;
 let currentWeather;
 let forecastData;
+
+// Add history to local storage
+const historyStorage = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
 // Activate search button
 document.querySelectorAll("button").forEach((button) => {
   button.classList.add("button");
 });
 function getweatherInfo(lat, lon) {
-  const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKEY}`;
+  const weatherURL = `${BASE_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKEY}`;
 
   fetch(weatherURL)
     .then(function (response) {
@@ -94,8 +28,19 @@ function getweatherInfo(lat, lon) {
       console.log("This is currect weather DATA: ", data);
     });
 }
+// submit form information
+document.querySelector("form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  let cityName = document.getElementById("citySearch").value;
+
+  getCoords(cityName);
+
+  // renderCoords(cityName, forecastData);
+});
+// API call api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 function getCoords(city) {
-  const coordsURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKEY}`;
+  const coordsURL = `${BASE_URL}/geo/1.0/direct?q=${city},US&limit=1&appid=${apiKEY}`;
 
   fetch(coordsURL)
     .then(function (response) {
@@ -107,25 +52,96 @@ function getCoords(city) {
       console.log("lat: ", data[0].lat);
       console.log("lon: ", data[0].lon);
       getweatherInfo(data[0].lat, data[0].lon);
+      data.forEach((cityName) => {
+        let html = `<p>${cityName.name}</p>`;
+        document.getElementById("current").innerHTML = html;
+        document.getElementById("demo").innerHTML = "weatherData";
+
+        // let temp = `<li>${weather.main}</li>`;
+        // temp.id = "currentWeather";
+        // currentWeather.innerHTML =
+        //   `<b>Temperature:</b>` + currentWeather + "°F";
+        // currentWeather.appendchild(temp);
+        // document.querySelector("ol").innerHTML = html;
+      });
     });
 }
-// submit form information
-document.querySelector("form").addEventListener("submit", async (event) => {
-  event.preventDefault();
+// Show weather on webpage
+// Create element for city name
 
-  let cityName = document.getElementById("citySearch").value;
+// let temp = document.createElement("p");
+// temp.id = "currentWeather";
+// let current = `<b>Temperature:</b> ${currentWeather}°F`;
+// document.getElementById("current").appendChild(node);
+// Weather header when weather is displayed on the app
+// function createCurrentWeatherHeader({ cityName, icon }) {
+// const header = document.createElement("header");
+// const title = document.createElement("h2");
+// const iconEl = document.createElement("img");
 
-  getCoords(cityName);
+// icon.alt = description;
 
-  renderCoords(cityName, forecastData);
-});
+// header.appendchild(title);
+// header.appendChild(iconEl);
 
-document
-  .getElementById("cityResults")
-  .addEventListener("click", async (event) => {
-    if (event.target.searchHistory === "button") {
-      let cityResults = event.target.innerText.toUppercase();
+// return header;
+// }
 
-      let dataEl = await apiService.getWeatherByCity(cityResults);
-    }
-  });
+// function createWeatherH2() {
+//   const h2 = document.createElement("h2");
+//   h2.textContent = "5-Day Forecast";
+
+//   return h2;
+// }
+
+// function createForecastList(forecastData) {}
+
+// function createWeatherList({ temp, windSpeed, humidity }) {}
+
+// function renderCurrentWeather(weather, el) {
+//   el.innerHTML = "";
+//   // destructuring and renaming object
+//   const { name: cityName, main, weather: weatherData } = weather;
+//   const { temp, humidity } = main;
+//   const { description, icon } = weatherData[0];
+//   const { speed: windSpeed } = weather.wind;
+
+//   el.appendchild(createCurrentWeatherHeader({ cityName, description, icon }));
+//   el.appendchild(createWeatherList({ temp, windSpeed, humidity }));
+//   // tailwind to give top to bottom effect
+//   // el.classList.add("border-y");
+// }
+
+// search and display the current weather by grabbing searched city from search box and adding to search history
+// function addCityToHistoryStorage(cityName, coords) {
+//   // to avoid duplicate city search in history section
+//   const cityNameUppercase = cityName.toUppercase();
+//   if (!historyStorage.find((city) => city.name === cityNameUppercase)) {
+//     historyStorage.push({ name: cityNameUppercase, coords });
+//     localStorage.setItem("searchHistory", JSON.stringify(historyStorage));
+//   }
+// }
+
+// async function renderForecast(coords) {
+//   currentWeather = await apiService.getCurrentWeather(coords);
+//   forecastData = await apiService.getForecastData(coords);
+
+//   renderWeather(currentWeather, currentWeatherSection);
+//   renderCurrentWeather(forecastData, forecastSection);
+// }
+
+// renderHistoryButtons(historyStorage, searchHistorySection);
+
+// form.addEventListener("submit", async (event) => {});
+
+// searchHistorySection.addEventListener("click", async (event) => {});
+
+// document
+//   .getElementById("cityResults")
+//   .addEventListener("click", async (event) => {
+//     if (event.target.searchHistory === "button") {
+//       let cityResults = event.target.innerText.toUppercase();
+
+//       let dataEl = await apiService.getWeatherByCity(cityResults);
+//     }
+//   });
