59fdf6c41137aa4fb814188f470791dadc871b3e
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-12-08 17:15:26 -0600

    Added script tags

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,14 +1,19 @@
+// Used day.js to format date
+let date = dayjs().format('M/DD/YYYY');
+dayjs().format('M/D/YYYY')
+
 const form = document.querySelector("form");
 const searchHistorySection = document.getElementById("searchHistory");
 const forecastSection = document.getElementById("forecast");
 const currentWeatherSection = document.getElementById("currentWeather");
 const apiKEY = "e44adac92450ec69c542cdd83e3a48b0";
 const BASE_URL = "https://api.openweathermap.org";
+const cityResults = document.getElementById("cityResults");
 
 let prevCity = "";
 let coords;
-let currentWeather;
-let forecastData;
+let currentWeatherIconUrl = "";
+let humidityValue = "";
 
 // Add history to local storage
 const historyStorage = JSON.parse(localStorage.getItem("searchHistory")) || [];
@@ -25,13 +30,72 @@ function getweatherInfo(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      console.log("This is currect weather DATA: ", data);
+      console.log("This is current weather DATA: ", data);
+      console.log(data.list);
+      let list = document.createElement('div');
+      // list.innerHTML = list[0];
+      // current.appendChild(list)
+      // document.getElementById("demo").innerHTML = data[0];
+      console.log(data.list[0]);
+      console.log(data.list[0].dt);
+      console.log(data.list[0].main.temp);
+      console.log(data.list[0].main.humidity);
+      console.log(data.list[0].weather[0].icon);
+      console.log(data.list[0].wind.speed);
+      // Display current date
+      let dateEl = document.createElement('h2');
+      dateEl.textContent = data.list[0].dt;
+      list.appendChild(dateEl);
+
+      //Display current weather icon
+      let iconEl = document.createElement('img');
+      iconEl.src = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png"
+      list.appendChild(iconEl);
+
+      // Display temperature in Fahrenheit
+      let temp = document.createElement('p');
+      temp.textContent = `Temperature: `+ data.list[0].main.temp + `°F`
+      list.appendChild(temp);
+
+      // Display windspeed in MPH
+      let windSpeed = document.createElement('p');
+      windSpeed.textContent = `WindSpeed: ` + data.list[0].wind.speed + `MPH`
+      list.appendChild(windSpeed);
+
+      // Display humidity in %
+      let humidityValue = document.createElement('p');
+      humidityValue.textContent = `Humidity: `+ data.list[0].main.humidity + `%`
+      list.appendChild(humidityValue);
+
+
+      console.log(list);
+      let ul = document.getElementById("demo");
+      ul.appendChild(list);
+      currentWeatherSection.append(ul);
+
     });
+
 }
+
+// set function add search to history section
+// const cityNameUppercase = citySearch.toUppercase();
+function saveSearch() {
+  const cityInputSearch = citySearch.value;
+  historyStorage.push({ city: cityInputSearch });
+  localStorage.setItem("searchHistory", JSON.stringify(historyStorage));
+  for (let i = 0; i < historyStorage.length; i++) {
+    let ul = document.createElement("ul");
+    ul.textContent = `${historyStorage[i].city}`;
+    cityResults.append(ul);
+  }
+}
+
+
+
 // submit form information
 document.querySelector("form").addEventListener("submit", async (event) => {
   event.preventDefault();
-
+  saveSearch();
   let cityName = document.getElementById("citySearch").value;
 
   getCoords(cityName);
@@ -53,27 +117,19 @@ function getCoords(city) {
       console.log("lon: ", data[0].lon);
       getweatherInfo(data[0].lat, data[0].lon);
       data.forEach((cityName) => {
-        let html = `<p>${cityName.name}</p>`;
-        document.getElementById("current").innerHTML = html;
-        document.getElementById("demo").innerHTML = "weatherData";
-
-        // let temp = `<li>${weather.main}</li>`;
-        // temp.id = "currentWeather";
-        // currentWeather.innerHTML =
-        //   `<b>Temperature:</b>` + currentWeather + "°F";
-        // currentWeather.appendchild(temp);
-        // document.querySelector("ol").innerHTML = html;
+        let div = `<p>${cityName.name}</p>`;
+        document.getElementById("current").innerHTML = div;
       });
     });
-}
-// Show weather on webpage
-// Create element for city name
+};
 
-// let temp = document.createElement("p");
-// temp.id = "currentWeather";
-// let current = `<b>Temperature:</b> ${currentWeather}°F`;
-// document.getElementById("current").appendChild(node);
-// Weather header when weather is displayed on the app
+
+
+
+
+
+
+// // Weather header when weather is displayed on the app
 // function createCurrentWeatherHeader({ cityName, icon }) {
 // const header = document.createElement("header");
 // const title = document.createElement("h2");
@@ -85,7 +141,7 @@ function getCoords(city) {
 // header.appendChild(iconEl);
 
 // return header;
-// }
+// } //undefined
 
 // function createWeatherH2() {
 //   const h2 = document.createElement("h2");
@@ -96,43 +152,21 @@ function getCoords(city) {
 
 // function createForecastList(forecastData) {}
 
-// function createWeatherList({ temp, windSpeed, humidity }) {}
+// function createWeatherList({ temperature, windSpeed, humidity }) {}
 
-// function renderCurrentWeather(weather, el) {
+// function currentWeather(weather, el) {
 //   el.innerHTML = "";
 //   // destructuring and renaming object
 //   const { name: cityName, main, weather: weatherData } = weather;
-//   const { temp, humidity } = main;
+//   const { temperature, humidity } = main;
 //   const { description, icon } = weatherData[0];
 //   const { speed: windSpeed } = weather.wind;
 
 //   el.appendchild(createCurrentWeatherHeader({ cityName, description, icon }));
-//   el.appendchild(createWeatherList({ temp, windSpeed, humidity }));
+//   el.appendchild(createWeatherList({ temperature, windSpeed, humidity }));
 //   // tailwind to give top to bottom effect
 //   // el.classList.add("border-y");
-// }
-
-// search and display the current weather by grabbing searched city from search box and adding to search history
-// function addCityToHistoryStorage(cityName, coords) {
-//   // to avoid duplicate city search in history section
-//   const cityNameUppercase = cityName.toUppercase();
-//   if (!historyStorage.find((city) => city.name === cityNameUppercase)) {
-//     historyStorage.push({ name: cityNameUppercase, coords });
-//     localStorage.setItem("searchHistory", JSON.stringify(historyStorage));
-//   }
-// }
-
-// async function renderForecast(coords) {
-//   currentWeather = await apiService.getCurrentWeather(coords);
-//   forecastData = await apiService.getForecastData(coords);
-
-//   renderWeather(currentWeather, currentWeatherSection);
-//   renderCurrentWeather(forecastData, forecastSection);
-// }
-
-// renderHistoryButtons(historyStorage, searchHistorySection);
-
-// form.addEventListener("submit", async (event) => {});
+// } //no errors; undefined
 
 // searchHistorySection.addEventListener("click", async (event) => {});
 
