6ff13ba755b84824ebe1a5863645a68499ea6ced
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-01-09 18:29:33 -0600

    Create rewrite file to clean up code

--- assets/src/script.js
+++ assets/src/script.js
@@ -29,6 +29,25 @@ document.getElementById("searchBtn").addEventListener("click", function () {
   localStorage.setItem("searchHistory", JSON.stringify(historyStorage))
 });
 
+// Activate clear history button
+document.getElementById("clearHistory").addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+});
+
+// Activate search history
+historyStorage.forEach((city) => {
+  let ul = document.createElement("ul");
+  ul.textContent = `${city.city}`;
+  cityResults.append(ul);
+});
+
+// Activate searched city as button
+document.querySelector("ul").addEventListener("click", function (event) {
+  let citySearch = event.target.textContent;
+  coords(citySearch);
+});
+
 // Function to get the coordinates of the searched city
 function getCoords(city) {
   const coordsURL = `${BASE_URL}/geo/1.0/direct?q=${city}&limit=5&appid=${apiKEY}`;
@@ -62,77 +81,56 @@ function getWeatherInfo(lat, lon) {
       console.log("This is current weather DATA: ", data);
       console.log(data.list);
       let list = document.createElement("div");
-      //       // list.innerHTML = list[0];
-//       // current.appendChild(list)
-//       // document.getElementById("demo").innerHTML = data[0];
-         // console.log(data.list[0]);
-      // console.log(data.list[0].dt);
-      // console.log(data.list[0].main.temp);
-      // console.log(data.list[0].main.humidity);
-      // console.log(data.list[0].weather[0].icon);
-      // console.log(data.list[0].wind.speed);
-      //       // Display current date
-//       let dateEl = document.createElement('h2');
-//       dateEl.textContent = data.list[0].dt;
-//       list.appendChild(dateEl);
+      // list.innerHTML = list[0];
+      // current.appendChild(list)
+      // document.getElementById("demo").innerHTML = data[0];
+      console.log(data.list[0]);
+      console.log(data.list[0].dt);
+      console.log(data.list[0].main.temp);
+      console.log(data.list[0].main.humidity);
+      console.log(data.list[0].weather[0].icon);
+      console.log(data.list[0].wind.speed);
 
-      // //Display current weather icon
-      // let iconEl = document.createElement('img');
-      // iconEl.src = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png"
-      // list.appendChild(iconEl);
+      //Display current weather icon
+      let iconEl = document.createElement('img');
+      iconEl.src = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png"
+      list.appendChild(iconEl);
 
-      //       // Display temperature in Fahrenheit
-//       let temp = document.createElement('p');
-//       temp.textContent = `Temperature: `+ data.list[0].main.temp + `°F`
-//       list.appendChild(temp);
+      // Display temperature in Fahrenheit
+      let temp = document.createElement('p');
+      temp.textContent = `Temperature: `+ data.list[0].main.temp + `°F`
+      list.appendChild(temp);
 
-//       // Display windspeed in MPH
-//       let windSpeed = document.createElement('p');
-//       windSpeed.textContent = `WindSpeed: ` + data.list[0].wind.speed + `MPH`
-//       list.appendChild(windSpeed);
+      // Display windspeed in MPH
+      let windSpeed = document.createElement('p');
+      windSpeed.textContent = `WindSpeed: ` + data.list[0].wind.speed + `MPH`
+      list.appendChild(windSpeed);
 
-//       // Display humidity in %
-//       let humidityValue = document.createElement('p');
-//       humidityValue.textContent = `Humidity: `+ data.list[0].main.humidity + `%`
-//       list.appendChild(humidityValue);
+      // Display humidity in %
+      let humidityValue = document.createElement('p');
+      humidityValue.textContent = `Humidity: `+ data.list[0].main.humidity + `%`
+      list.appendChild(humidityValue);
 
-
-//       console.log(list);
-//       let ul = document.getElementById("demo");
-//       ul.appendChild(list);
-//       currentWeatherSection.append(ul);
+      console.log(list);
+      let ul = document.getElementById("demo");
+      ul.appendChild(list);
+      currentWeatherSection.append(ul);
     });
 }
 
-// Activate clear history button
-document.getElementById("clearHistory").addEventListener("click", function () {
-  localStorage.clear();
-  location.reload();
-});
 
-// Activate search history
-historyStorage.forEach((city) => {
-  let ul = document.createElement("ul");
-  ul.textContent = `${city.city}`;
-  cityResults.append(ul);
-});
-
-// Activate searched city as button
-cityResults.addEventListener("click", function (event) {
-  let citySearch = event.target.textContent;
-  getCoords(citySearch);
-});
 
 // submit form information
-// document.querySelector("form").addEventListener("submit", async (event) => {
-//   event.preventDefault();
-//   saveSearch();
-//   let cityName = document.getElementById("citySearch").value;
+document.querySelector("form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  saveSearch();
+  let cityName = document.getElementById("citySearch").value;
 
-//   getCoords(cityName);
+  coords(cityName);
 
-  // renderCoords(cityName, forecastData);
-// });
+  renderCoords(cityName, forecastData);
+});
+
 // API call api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 // function getCoords(city) {
 //   const coordsURL = `${BASE_URL}/geo/1.0/direct?q=${city},US&limit=1&appid=${apiKEY}`;
@@ -159,17 +157,30 @@ cityResults.addEventListener("click", function (event) {
 
 // TODO: Create a function to display current weather
 
+
+
 // TODO: Create a function to display 5-day forecast
+
+
 
 //  TODO: Create a function to display searched city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 
+
+
 // TODO: Create a function to display searched city's 5-day forecast with date, an icon representation of weather conditions, the temperature, the windspeed,and the humidity
+
+
 
 // TODO: Create a function to render the weather data to the page
 
+
 // TODO: Create a function to render the 5-day forecast to the page
 
+
+
 // TODO: Create a function to render the search history to the page
+
+
 
 
 
