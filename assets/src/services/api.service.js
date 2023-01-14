fc8f81cee335e5b863b2eb332a796dd4d6d6a42b
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-01-13 20:05:55 -0600

    Created API service page to get coords

--- assets/src/services/api.service.js
+++ assets/src/services/api.service.js
@@ -1,33 +1,38 @@
-// const apiKEY = "e44adac92450ec69c542cdd83e3a48b0";
-// const BASE_URL = "https://api.openweathermap.org";
-const apiCoords = `${BASE_URL}/geo/1.0/direct`;
-const apiWeather = `${BASE_URL}/data/2.5/weather`;
-const apiForecast = `${BASE_URL}/data/2.5/forecast`;
+// API Serivce: http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=e44adac92450ec69c542cdd83e3a48b0
 
-// API call api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+const API_KEY = "e44adac92450ec69c542cdd83e3a48b0";
+const BASE_URL = "https://api.openweathermap.org";
+const API_COORDS = `${BASE_URL}/geo/1.0/direct`;
+const API_WEATHER = `${BASE_URL}/data/2.5/weather`;
+const API_FORECAST = `${BASE_URL}/data/2.5/forecast`;
+
 export default {
-  getWeatherResponse(cityName) {
-    return fetch(
-      `${BASE_URL}/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKEY}`
-    );
-  },
   async getCoords(city) {
     const response = await fetch(
-      `${apiCoords}?q=${city}&limit=5&appid=${apiKEY}`
+      `${API_COORDS}?q=${city}&limit=5&appid=${API_KEY}`
     );
     const [initialCity] = await response.json();
     return { lat: initialCity.lat, lon: initialCity.lon };
   },
-  async getCurrentWeather(weather) {
+
+  async getCurrentWeather(coords) {
     const response = await fetch(
-      `${apiWeather}?lat=${weather.lat}&lon=${weather.lon}&units=imperial&appid=${apiKEY}`
+      `${API_WEATHER}?lat=${coords.lat}&lon=${coords.lon}&units=imperial&appid=${API_KEY}`
     );
+    // The caller will await kthe Promise object; no need to wait
     return await response.json();
   },
   async getForecastData(coords) {
     const response = await fetch(
-      `${apiForecast}?lat=${coords.lat}&lon=${coords.lon}&units=imperial&appid=${apiKEY}`
+      `${API_FORECAST}?lat=${coords.lat}&lon=${coords.lon}&units=imperial&appid=${API_KEY}`
     );
-    return await response.json();
+    // Since there's 40 entries on 'list' array, we need to filter it to get only the ones that are at 12:00:00
+    const data = await response.json();
+    return data.list.filter(
+      (
+        // Object destructuring and renaming
+        { dt_txt: dateTxt }
+      ) =>dateTxt.endsWith("12:00:00")
+    );
   },
 };
