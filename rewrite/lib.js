1fc438c4d53d6284de896199862eb82953cd5295
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-01-13 21:10:29 -0600

    Export query selector and element id

---
+++ rewrite/lib.js
@@ -0,0 +1,4 @@
+export const form = document.querySelector("form");
+export const searchHistorySection = document.getElementById("searchHistory");
+export const todaySection = document.getElementById("current");
+export const forecastSection = document.getElementById("forecast");
