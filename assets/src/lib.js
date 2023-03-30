d9fc4491664455cd58eafa0dc92ebbb88c504bff
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-03-29 22:34:40 -0500

    Remove old broken code

--- assets/src/lib.js
+++
@@ -1,4 +0,0 @@
-export const form = document.querySelector("form");
-export const searchHistorySection = document.getElementById("searchHistory");
-export const todaySection = document.getElementById("current");
-export const forecastSection = document.getElementById("forecast");
